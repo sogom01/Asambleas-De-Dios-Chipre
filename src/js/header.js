28a6a3c0ca1973dc0a75832a3df97dec7fff7067
Juan Sebastián Osorio Gómez <jsosorio69619@icloud.com>
2024-06-25 20:51:55 -0500

    Cambios en la responsividad de la barra de navegación

--- src/js/header.js
+++ src/js/header.js
@@ -1,13 +1,48 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const menuButton = document.getElementById("mobile-menu-button");
-    const closeMenuButton = document.getElementById("close-menu-button");
-    const mobileMenu = document.getElementById("mobile-menu");
+document.addEventListener("DOMContentLoaded", () => {
+    const scheduleButton = document.getElementById("scheduleButton");
+    const scheduleButtonMobile = document.getElementById("scheduleButtonMobile");
+    const scheduleModal = document.getElementById("scheduleModal");
+    const closeModalButton = document.getElementById("closeModalButton");
 
-    menuButton.addEventListener("click", function () {
+    scheduleButton.addEventListener("click", () => {
+        scheduleModal.classList.remove("hidden");
+    });
+
+    closeModalButton.addEventListener("click", () => {
+        scheduleModal.classList.add("hidden");
+    });
+
+    window.addEventListener("click", (event) => {
+        if (event.target === scheduleModal) {
+            scheduleModal.classList.add("hidden");
+        }
+    });
+
+    scheduleButtonMobile.addEventListener("click", () => {
+        scheduleModal.classList.remove("hidden");
+    });
+
+    closeModalButton.addEventListener("click", () => {
+        scheduleModal.classList.add("hidden");
+    });
+
+    window.addEventListener("click", (event) => {
+        if (event.target === scheduleModal) {
+            scheduleModal.classList.add("hidden");
+        }
+    });
+
+
+
+    const mobileMenuButton = document.getElementById("mobile-menu-button");
+    const mobileMenu = document.getElementById("mobile-menu");
+    const closeMobileMenuButton = document.getElementById("close-menu-button");
+
+    mobileMenuButton.addEventListener("click", () => {
         mobileMenu.classList.toggle("hidden");
     });
 
-    closeMenuButton.addEventListener("click", function () {
-        mobileMenu.classList.toggle("hidden");
+    closeMobileMenuButton.addEventListener("click", () => {
+        mobileMenu.classList.add("hidden");
     });
 });
