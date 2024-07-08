3fa2260b95df81844b63ae3ecf5f9fd2be12035a
Juan Sebastián Osorio Gómez <jsosorio69619@icloud.com>
2024-07-08 14:40:44 -0500

    refactor: Remove unused import of SpeedInsights in Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,4 @@
 import { defineConfig } from 'astro/config';
-import SpeedInsights from "@vercel/speed-insights/astro"
-import vercel from '@astrojs/vercel/serverless';
 
-export default defineConfig({
-    output: 'server',
-    adapter: vercel({
-        webAnalytics: { enabled: true }
-    }),
-});
+// https://astro.build/config
+export default defineConfig({});
